5d9287115b2e61322a354068879f103ff16189d2
Ravindra5F7 <raveendra.siraparapu@sasi.ac.in>
2024-07-26 18:08:20 +0530

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import Signin from './pages/signin';
+import Signup from './pages/signup';
+import Profile from './pages/profile';
+import Home from './pages/home';
+import Aboutus from './pages/about';
 export default function App() {
   return (
-    <h1 className='text-blue-700'>ravi</h1>
+    <BrowserRouter className='text-blue-700'>
+      <Routes>
+        <Route path='/home' Component={<Home/>}/>
+        <Route path='/about' element={<Aboutus/>} />
+        <Route path='/sign-in' element={<Signin/>} />
+        <Route path='/sign-up' element={<Signup/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
